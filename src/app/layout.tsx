e57d54fa1a6bfb712da02e014afbd09ae4b756e5
Mitch <mitchjoelklein@hotmail.com>
2025-03-10 13:49:33 +1100

    deleted favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,7 @@ export const metadata: Metadata = {
     default:
       'Mitchell Klein - Software designer, founder, and amateur astronaut',
   },
-  description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+  description: 'I’m Mitch, a Sydney-based software developer.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
